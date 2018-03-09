5473b2aa42b24d120500c41334c3162062739253
Zach Mullen <zach.mullen@kitware.com>
2018-03-08 19:00:08 -0500

    Add PV glance viewer for CT visualization

---
+++ src/views/CT.vue
@@ -0,0 +1,13 @@
+<script>
+import { createViewer } from 'paraview-glance';
+
+export default {
+  mounted() {
+    this.glance = createViewer(this.$el);
+  },
+  destroyed() {
+    this.glance.destroy();
+  },
+  template: '<div/>',
+};
+</script>
