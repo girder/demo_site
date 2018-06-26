bff932bb02079e93586fce2da6570ea9c89aad41
Zach Mullen <zach.mullen@kitware.com>
2018-06-26 13:52:56 -0400

    Load ITK readers statically

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,13 +1,11 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
-const autoprefixer = require('autoprefixer')
+const path = require('path');
+const utils = require('./utils');
+const config = require('../config');
+const vueLoaderConfig = require('./vue-loader.conf');
+const autoprefixer = require('autoprefixer');
+const CopyPlugin = require('copy-webpack-plugin');
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
-}
+const resolve = dir => path.join(__dirname, '..', dir);
 
 const createLintingRule = () => ({
   test: /\.(js|vue)$/,
@@ -16,14 +14,14 @@ const createLintingRule = () => ({
   include: [resolve('src'), resolve('test')],
   options: {
     formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
+    emitWarning: !config.dev.showEslintErrorsInOverlay,
+  },
 })
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: ['./src/main.js']
+    app: ['./src/main.js'],
   },
   output: {
     path: config.build.assetsRoot,
@@ -31,14 +29,14 @@ module.exports = {
     libraryTarget: 'umd',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.dev.assetsPublicPath,
   },
   resolve: {
     extensions: ['.js', '.vue'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      vue$: 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-    }
+    },
   },
   module: {
     rules: [
@@ -46,7 +44,7 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: vueLoaderConfig
+        options: vueLoaderConfig,
       },
       {
         test: /\.js$/,
@@ -55,30 +53,31 @@ module.exports = {
           resolve('src'),
           resolve('test'),
           resolve('node_modules/webpack-dev-server/client'),
-          resolve('node_modules/paraviewweb/src')]
+          resolve('node_modules/paraviewweb/src')
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 60000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath('media/[name].[hash:7].[ext]'),
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
         }
       },
       {
@@ -120,7 +119,7 @@ module.exports = {
           },
         ],
       },
-    ]
+    ],
   },
   node: {
     // prevent webpack from injecting mocks to Node native modules
@@ -129,8 +128,14 @@ module.exports = {
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
-    child_process: 'empty'
+    child_process: 'empty',
   },
   plugins: [
+    new CopyPlugin([
+      {
+        from: path.join('node_modules', 'itk'),
+        to: 'itk',
+      },
+    ]),
   ],
-}
+};
--- src/views/Glance.vue
+++ src/views/Glance.vue
@@ -5,6 +5,7 @@ glance-app(ref="glance")
 <script>
 import Vue from 'vue';
 import GlanceApp from 'paraview-glance/src/components/core/App';
+import ReaderFactory from 'paraview-glance/src/io/ReaderFactory';
 import Config from 'paraview-glance/src/config';
 import { Widgets } from 'paraview-glance/src/constants';
 import CropWidget from 'paraview-glance/src/vtkwidgets/CropWidget';
@@ -12,6 +13,23 @@ import vtkListenerHelper from 'paraview-glance/src/ListenerHelper';
 import vtkWidgetManager from 'paraview-glance/src/vtkwidgets/WidgetManager';
 import vtkProxyManager from 'vtk.js/Sources/Proxy/Core/ProxyManager';
 import 'paraview-glance/src/io/ParaViewGlanceReaders';
+
+import vtkITKImageReader from 'vtk.js/Sources/IO/Misc/ITKImageReader';
+
+import extensionToImageIO from 'itk/extensionToImageIO';
+import readImageArrayBuffer from 'itk/readImageArrayBuffer';
+
+
+vtkITKImageReader.setReadImageArrayBufferFromITK(readImageArrayBuffer);
+new Set(Object.keys(extensionToImageIO).map(ext => ext.toLowerCase())).forEach((ext) => {
+  ReaderFactory.registerReader({
+    extension: ext,
+    name: `${ext.toUpperCase()} Reader`,
+    vtkReader: vtkITKImageReader,
+    binary: true,
+    fileNameMethod: 'setFileName',
+  });
+});
 
 Vue.prototype.$globalBus = new Vue();
 
