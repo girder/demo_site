5e1e022511f0ff46be001f8ece17cd02fc720262
Zach Mullen <zach.mullen@kitware.com>
2018-07-19 14:42:41 -0400

    Remove outdated code

--- src/views/Glance.vue
+++ src/views/Glance.vue
@@ -4,17 +4,11 @@ glance-app
 
 <script>
 import Vue from 'vue';
-import { mapActions } from 'vuex';
+import Vuex, { mapActions } from 'vuex';
 import GlanceApp from 'paraview-glance/src/components/core/App';
 import ReaderFactory from 'paraview-glance/src/io/ReaderFactory';
-import Config from 'paraview-glance/src/config';
-import { Widgets } from 'paraview-glance/src/constants';
-import CropWidget from 'paraview-glance/src/vtkwidgets/CropWidget';
-import vtkListenerHelper from 'paraview-glance/src/ListenerHelper';
-import vtkWidgetManager from 'paraview-glance/src/vtkwidgets/WidgetManager';
-import Store from 'paraview-glance/src/stores';
+import createStore from 'paraview-glance/src/stores';
 import StoreTypes from 'paraview-glance/src/stores/types';
-import vtkProxyManager from 'vtk.js/Sources/Proxy/Core/ProxyManager';
 import 'paraview-glance/src/io/ParaViewGlanceReaders';
 
 import vtkITKImageReader from 'vtk.js/Sources/IO/Misc/ITKImageReader';
@@ -22,6 +16,7 @@ import vtkITKImageReader from 'vtk.js/Sources/IO/Misc/ITKImageReader';
 import extensionToImageIO from 'itk/extensionToImageIO';
 import readImageArrayBuffer from 'itk/readImageArrayBuffer';
 
+Vue.use(Vuex);
 
 vtkITKImageReader.setReadImageArrayBufferFromITK(readImageArrayBuffer);
 new Set(Object.keys(extensionToImageIO).map(ext => ext.toLowerCase())).forEach((ext) => {
@@ -33,24 +28,6 @@ new Set(Object.keys(extensionToImageIO).map(ext => ext.toLowerCase())).forEach((
     fileNameMethod: 'setFileName',
   });
 });
-
-Vue.prototype.$globalBus = new Vue();
-
-const proxyConfiguration = Config.Proxy;
-const proxyManager = vtkProxyManager.newInstance({ proxyConfiguration });
-const renderListener = vtkListenerHelper.newInstance(
-  proxyManager.autoAnimateViews,
-  () => [
-    ...proxyManager.getSources(),
-    ...proxyManager.getRepresentations(),
-    ...proxyManager.getViews(),
-  ],
-);
-
-proxyManager.onProxyRegistrationChange(renderListener.resetListeners);
-
-const widgetManager = vtkWidgetManager.newInstance({ proxyManager });
-widgetManager.registerWidgetGroup(Widgets.CROP, CropWidget);
 
 export default {
   components: { GlanceApp },
@@ -64,11 +41,7 @@ export default {
       default: null,
     },
   },
-  provide: {
-    proxyManager,
-    widgetManager,
-  },
-  store: Store,
+  store: createStore(),
   watch: {
     url() {
       this.openFiles({
